d3c696cb52108b092dbd161fb09119b9a0f283b6
Alberto <ing_aperezr@yahoo.com>
2018-07-31 12:08:10 -0500

    Añadidos comportamientos diversos para GET, POST, PUT y DELETE

--- server/server.js
+++ server/server.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', function(req, res) {
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', function(req, res) {
     //res.send('Hello World!')      // Respuesta HTML
-    res.json('Hello, World!') // Respuesta JSON
+    res.json('get Usuario') // Respuesta JSON
+});
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+    //res.send('Hello World!')      // Respuesta HTML
+    res.json({
+        persona: body
+    }); // Respuesta JSON
+});
+app.put('/usuario/:id', function(req, res) {
+    //res.send('Hello World!')      // Respuesta HTML
+    let id = req.params.id;
+    res.json({
+        id
+    }); // Respuesta JSON
+});
+app.delete('/usuario', function(req, res) {
+    //res.send('Hello World!')      // Respuesta HTML
+    res.json('delete Usuario') // Respuesta JSON
 });
 
 app.listen(3000, () => {
