47d96808c5d357da1c311edb6e35644e039417bc
Alberto <ing_aperezr@yahoo.com>
2018-07-31 17:01:21 -0500

    Ambientes de Desarrollo y Producción definidos

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,20 @@
 //===========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ==========================
+// Entorno
+//===========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==========================
+// Base de datos
+//===========================
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-admin:URLB9tIeocI6odLrCDL6@ds161391.mlab.com:61391/cafe';
+}
+
+process.env.URLDB = urlDB;
